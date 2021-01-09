0f796a3b5512e2c9defd5678ffed19ba92e61070
Gustav Utke Kauman <gustav.kauman@gmail.com>
2021-01-09 18:38:35 +0100

    Add register method

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', [\App\Http\Controllers\UserController::class, 'store'])->name('api.user.store');
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
