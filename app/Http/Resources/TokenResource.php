4bd454d7a7ce0ef1cf9b583022c2fda2272e8d09
Gustav Utke Kauman <gustav.kauman@gmail.com>
2021-01-10 18:19:20 +0100

    Fix data conversion

--- app/Http/Resources/TokenResource.php
+++ app/Http/Resources/TokenResource.php
@@ -14,6 +14,8 @@ class TokenResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'data' => $this->resource
+        ];
     }
 }
