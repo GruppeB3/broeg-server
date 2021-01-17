8c8ebf50b7ff03c6fc2558be63c3c837f64af7a4
Gustav Utke Kauman <gustav.kauman@gmail.com>
2021-01-17 23:04:27 +0100

    Add error message

--- app/Http/Controllers/ApiV1/UserController.php
+++ app/Http/Controllers/ApiV1/UserController.php
@@ -77,13 +77,12 @@ class UserController extends Controller
             'email' => 'required|email',
         ]);
 
-        // We will send the password reset link to this user. Once we have attempted
-        // to send the link, we will examine the response then see the message we
-        // need to show to the user. Finally, we'll send out a proper response.
         $status = Password::sendResetLink(
             $request->only('email')
         );
 
-        return $status;
+        return $status == Password::RESET_LINK_SENT
+            ? json_encode(['message' => 'Reset request successfully sent'])
+            : json_encode(['message' => 'Failed to send reset request']);
     }
 }
